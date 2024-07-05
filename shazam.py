ba3a5458790c5ab0219f78376d6913631a70bfee
maxdunitz <33816834+maxdunitz@users.noreply.github.com>
2024-07-05 17:16:49 +0200

    Add files via upload
    update

--- shazam.py
+++ shazam.py
@@ -7,12 +7,12 @@ async def main():
   D = await shazam.recognize('capture.mp3')  
   album_info = [f"{d['title']}: {d['text']}" for d in D['track']['sections'][0]['metadata']]
   stream_link  = [d for d in D['track']['hub']['actions'] if d['type'] == 'uri'][0]['uri']
-  f = open("results.txt", "w")
+  f = open("templates/results.txt", "w")
   f.write(f"Title: {D['track']['title']}; Artist: {D['track']['subtitle']}\n")
   f.write(f"{'; '.join(album_info)}\n")
   f.write(f"Apple stream link: {stream_link}\n")
   tz = str(datetime.datetime.now().astimezone().tzinfo)
-  f.write(f"Last updated: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {tz}")
+  f.write(f'Last updated: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {tz}')
   f.close()
 
 loop = asyncio.get_event_loop()
