b1c8e128848f09fc4acfe547ccba7bb5c02bb4d6
maxdunitz <33816834+maxdunitz@users.noreply.github.com>
2024-10-18 17:29:13 +0200

    Update shazam.py
    writes to json

--- shazam.py
+++ shazam.py
@@ -1,19 +1,24 @@
 import asyncio
 from shazamio import Shazam
 import datetime
+import json
 
 async def main():
   shazam = Shazam()
-  D = await shazam.recognize('capture.mp3')  
-  album_info = [f"{d['title']}: {d['text']}" for d in D['track']['sections'][0]['metadata']]
-  stream_link  = [d for d in D['track']['hub']['actions'] if d['type'] == 'uri'][0]['uri']
-  f = open("templates/results.txt", "w")
-  f.write(f"Title: {D['track']['title']}; Artist: {D['track']['subtitle']}\n")
-  f.write(f"{'; '.join(album_info)}\n")
-  f.write(f"Apple stream link: {stream_link}\n")
+  D = await shazam.recognize('file.mp3')
+  album_info = dict([(f"{d['title']}", f"{d['text']}") for d in D['track']['sections'][0]['metadata']])
+  stream_link = [d for d in D['track']['hub']['actions'] if d['type'] == 'uri'][0]['uri']
   tz = str(datetime.datetime.now().astimezone().tzinfo)
-  f.write(f'Last updated: {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {tz}')
-  f.close()
+  info_dict = {
+               "Title": D['track']['title'], "Artist":D['track']['subtitle'],
+               "Apple Stream Link":stream_link,
+               "Last Updated":f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {tz}"
+               }
+  for k,v in album_info.items():
+    info_dict[k] = v
+
+  with open("templates/track_info.json", "w") as outfile:
+    outfile.write( json.dumps(info_dict) )
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
