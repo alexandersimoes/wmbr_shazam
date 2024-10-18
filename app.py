773a56b12cd18ad0632c691396feed2b30413e71
maxdunitz <33816834+maxdunitz@users.noreply.github.com>
2024-10-18 17:40:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("results.txt", mimetype='text/plain')
+    with open('templates/track_info.json', 'r') as file:
+        data = json.load(file)
 
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
